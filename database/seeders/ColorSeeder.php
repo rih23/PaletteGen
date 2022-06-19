6ca4ee6d5c3b87c81c5ce6f35d16e0554206e3d4
rih23 <rihardsblekus2002@gmail.com>
2022-06-19 12:28:42 +0300

    First iteration, only generating color palettes works.

---
+++ database/seeders/ColorSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Colors;
+
+class ColorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        Colors::truncate();
+
+        $csvFile = fopen(base_path("database/data/colornames.csv"), "r");
+
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
+            if (!$firstline) {
+                Colors::create([
+                    "colorName" => $data['0'],
+                    "hexCode" => $data['1']
+                ]);
+            }
+            $firstline = false;
+        }
+
+        fclose($csvFile);
+    }
+}
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+    <div class="flex bg-slate-800 space-x-4 text-white">
+            <a href="" class="ml-4">Home</a>
+            <a href="/generate">Generate</a>
+            <a href="">Palettes</a>
+            <div class="absolute right-0">
+            <a href=""">Register</a>
+            <a href="" class="px-4">Login</a>
+            </div>
+    </div>
+</div>
